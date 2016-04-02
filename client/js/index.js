52681b0a6da693aa9cc917bf85dc1d63c4674b23
NatanTechofNY <natan@techofny.com>
2016-04-02 09:54:18 -0400

    Added errors to client side

--- client/js/index.js
+++ client/js/index.js
@@ -7,7 +7,14 @@ if (Meteor.isClient) {
 	Template.indexItem.events({
 		'click #joinSessionBtn': function () {
 			var classCode = $('#codeInput').val();
+			var regCheck = /^[a-z0-9]+$/i;
+			if(typeof classCode !== 'string' && classCode.length !== 6)
+				Meteor.Error('Session ID must be 6 characters.');
+			else if (!classCode.math(regCheck))
+				Meteor.Error('Invalid session ID.');
+			else
 			Session.set('toJoinSession', classCode);
+
 		},
 		'click #createSessionBtn': function() {
 			Session.set('toCreateSession', true);	
@@ -18,15 +25,25 @@ if (Meteor.isClient) {
 			var fname = $('#firstNameInput').val();
 			var lname = $('#lastNameInput').val();
 			var stdntId = $('#studentId').val();
-			
+			var regCheckName = /^\d+$/;
+			if(regCheckName.test(fname) || regCheckName.test(lname) || fname.length < 2 || lname.length < 2)
+				Meteor.Error('Please enter a valid name.');
+			else if (studentId.length < 2)
+				Meteor.Error('Please enter a valid student ID');
 		},
 		'submit #createSessionForm': function(e) {
 			e.preventdefault();
-			var classCode = Session.get('toJoinSession');
 			var fname = $('#firstNameInput2').val();
 			var lname = $('#lastNameInput2').val();
 			var className = $('#classNameInput2').val();
 			var pin = $('#inputPassword').val();
+			var regCheck = /^\d+$/;
+			if(regCheck.test(fname) || regCheck.test(lname) || fname.length < 2 || lname.length < 2)
+				Meteor.Error('Please enter a valid name.');
+			else if (className.length < 2)
+				Meteor.Error('Class name must be longer then two characters');
+			else if(!regCheck.test(pin))
+				Meter.Error('Pin must only be numbers.')
 		}
 	});
 
