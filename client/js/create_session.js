79d045131ae1213dcb5e34288b8a35c9fbc192f0
peterTechofNY <peter@techofny.com>
2016-04-02 13:05:50 -0400

    mobile view

--- client/js/create_session.js
+++ client/js/create_session.js
@@ -1,4 +1,8 @@
 if (Meteor.isClient) {
-       
+       Template.create_session.events({
+       	'click #menu-toggle': function(e) {
+       		e.preventDefault();
+       		$('#wrapper').toggleClass("toggled");
+       	}
+       });
 }
-
