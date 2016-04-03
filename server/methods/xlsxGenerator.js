ea22899b11d399a929030b7d88858deebc4543f1
sum-kcid <jayp1924@gmail.com>
2016-04-03 00:08:07 -0400

    updates

--- server/methods/xlsxGenerator.js
+++ server/methods/xlsxGenerator.js
@@ -1,7 +1,21 @@
 if(Meteor.isServer) {
+	
+
+
 	Meteor.methods({
-		createSpreadsheet: function (usersList) {
+		createSpreadsheet: function (data) {
+			var userList = data.listed;
+			if (userList) {
+				userList = userList.map(function(g) {
+					return [g._id, g.fullName];
+				});
+
+				var buildr = [{name: "List for " + moment().format('LL'), data: userList}];
+
+				var toblob = XLSX.write(buildr, {bookType:'xlsx', bookSST:false, type:'binary'});
 			
+				return toblob;
+			};
 		}
 	});
 };
